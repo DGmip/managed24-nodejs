2c44176cd1912039d34db4181c436faa3f886264
Ben Wall <bwall256@gmail.com>
2019-02-13 15:02:30 +0000

    Allow 'port' param for running without docker Change the get to query the database with regex Add api key to search params Check for api key and searchterm

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongodb_1 = __importDefault(require("mongodb"));
 const cors_1 = __importDefault(require("cors"));
+const apiKey = 'd25d81b8-9986-4202-80da-b33a6c233580';
 // mongodb configuration
 const MongoClient = mongodb_1.default.MongoClient;
 const password = 'password25';
@@ -20,17 +21,79 @@ client.connect(err => {
     // client.close()
 });
 // expressjs configuration
-const port = 4220;
+let port = 8080;
 const host = '0.0.0.0';
+// take an argument to change the port for local development without docker
+const args = process.argv.slice(2);
+args.forEach((arg) => {
+    const split = arg.split('=');
+    const option = split[0];
+    const optionValue = parseInt(split[1]);
+    // console.log('vars', option, optionValue, split)
+    if (option === 'port') {
+        port = optionValue;
+    }
+});
+// configure the app
 const app = express_1.default();
+// set up cors for allowed origins
 app.use(cors_1.default({
     origin: [
+        // angular app port / docker angular port
         'http://localhost:4200',
+        // angular testing port
         'http://localhost:9876'
     ]
 }));
-// list the names in the db
+// names search function
+const searchNames = async function (searchTerm) {
+    const names = client.db('manage24').collection('names');
+    const cursor = names.find({ name: { $regex: searchTerm, $options: "$i" } });
+    try {
+        const result = await cursor.toArray();
+        console.log('result', result);
+        return Promise.resolve(result);
+    }
+    catch (err) {
+        console.error(err);
+        return Promise.reject(err);
+    }
+};
+const checkKey = function (key) {
+    return key === apiKey;
+};
+// names search endpoint
 app.get('/names', (req, res) => {
+    const searchTerm = req.query.search;
+    const key = req.query.apiKey;
+    // reject if there is no sufficient api key
+    if (!key || !checkKey(key)) {
+        const error = {
+            message: `You have supplied an invalid API key. Add your key to the 'key' paramater in the request URL, for example 'http://names-app.com/?key=<your-api-key>'`,
+            status: 403
+        };
+        res.status(403).send(error);
+    }
+    // reject if there is no searchterm
+    if (!searchTerm) {
+        const error = {
+            message: `There must be a parameter of 'search' on the request URL, for example 'http://names-app.com?search=milly'`,
+            status: 500
+        };
+        res.status(500).send(error);
+    }
+    // finally, search the names
+    searchNames(searchTerm)
+        .then((data) => {
+        console.log('got data');
+        res.status(200).send(data);
+    }, (error) => {
+        console.error('could not search the names', error.message);
+        res.status(500).send(error);
+    });
+});
+// list the names in the db - Old version created by mistake
+app.get('/all-names', (req, res) => {
     const names = client.db('manage24').collection('names');
     const cursor = names.find({});
     cursor.toArray()
@@ -68,54 +131,54 @@ app.get('/*', (req, res) => {
 app.listen(port, host);
 console.log('listening on ', port, host);
 const names = [
-    { name: "Paula Gowin" },
-    { name: "Lemuel Crown" },
-    { name: "Ellen Draughn" },
-    { name: "Alise Kaminsky" },
-    { name: "Sharleen Wesson" },
-    { name: "Janell Philpott" },
-    { name: "Verna Montanez" },
-    { name: "Rayna Branton" },
-    { name: "Paulita Perera" },
-    { name: "Priscila Pesce" },
-    { name: "Renae Pizano" },
-    { name: "Latarsha Mccallion" },
-    { name: "Myrna Millay" },
-    { name: "Marlen Gaynor" },
-    { name: "Mollie Ormand" },
-    { name: "Sunni Holtzman" },
-    { name: "Korey Vencill" },
-    { name: "Micheline Hopple" },
-    { name: "Regine Giron" },
-    { name: "Leonard Woodell" },
-    { name: "Bertie Guntrum" },
-    { name: "Frieda Weekly" },
-    { name: "Kendra Ragusa" },
-    { name: "Milly Shipman" },
-    { name: "Luciana Buttram" },
-    { name: "Maryjo Faulcon" },
-    { name: "Myriam Murrill" },
-    { name: "Nikia Carbonell" },
-    { name: "Estelle Anspach" },
-    { name: "Edie Delosreyes" },
-    { name: "Lorilee Knowles" },
-    { name: "Aracely Noakes" },
-    { name: "Alina Gould" },
-    { name: "Domitila Fralick" },
-    { name: "Amelia Wehner" },
-    { name: "Terrell Wilds" },
-    { name: "Cortney Vescio" },
-    { name: "Tommye Mcniel" },
-    { name: "Erich Hoy" },
-    { name: "Alissa Dunson" },
-    { name: "Dagmar Ayoub" },
-    { name: "Shane Bomba" },
-    { name: "Darcey Wulff" },
-    { name: "Robbie Tauber" },
-    { name: "Tawanna Cheek" },
-    { name: "Pamula Pettis" },
-    { name: "Paul Rucks" },
-    { name: "Lael Weber" },
-    { name: "Torrie Troupe" },
-    { name: "Etha Thornton" }
+    { name: 'Paula Gowin' },
+    { name: 'Lemuel Crown' },
+    { name: 'Ellen Draughn' },
+    { name: 'Alise Kaminsky' },
+    { name: 'Sharleen Wesson' },
+    { name: 'Janell Philpott' },
+    { name: 'Verna Montanez' },
+    { name: 'Rayna Branton' },
+    { name: 'Paulita Perera' },
+    { name: 'Priscila Pesce' },
+    { name: 'Renae Pizano' },
+    { name: 'Latarsha Mccallion' },
+    { name: 'Myrna Millay' },
+    { name: 'Marlen Gaynor' },
+    { name: 'Mollie Ormand' },
+    { name: 'Sunni Holtzman' },
+    { name: 'Korey Vencill' },
+    { name: 'Micheline Hopple' },
+    { name: 'Regine Giron' },
+    { name: 'Leonard Woodell' },
+    { name: 'Bertie Guntrum' },
+    { name: 'Frieda Weekly' },
+    { name: 'Kendra Ragusa' },
+    { name: 'Milly Shipman' },
+    { name: 'Luciana Buttram' },
+    { name: 'Maryjo Faulcon' },
+    { name: 'Myriam Murrill' },
+    { name: 'Nikia Carbonell' },
+    { name: 'Estelle Anspach' },
+    { name: 'Edie Delosreyes' },
+    { name: 'Lorilee Knowles' },
+    { name: 'Aracely Noakes' },
+    { name: 'Alina Gould' },
+    { name: 'Domitila Fralick' },
+    { name: 'Amelia Wehner' },
+    { name: 'Terrell Wilds' },
+    { name: 'Cortney Vescio' },
+    { name: 'Tommye Mcniel' },
+    { name: 'Erich Hoy' },
+    { name: 'Alissa Dunson' },
+    { name: 'Dagmar Ayoub' },
+    { name: 'Shane Bomba' },
+    { name: 'Darcey Wulff' },
+    { name: 'Robbie Tauber' },
+    { name: 'Tawanna Cheek' },
+    { name: 'Pamula Pettis' },
+    { name: 'Paul Rucks' },
+    { name: 'Lael Weber' },
+    { name: 'Torrie Troupe' },
+    { name: 'Etha Thornton' }
 ];
--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import mongodb from 'mongodb'
 import { Request, Response } from 'express'
 import cors from 'cors'
+import { Error } from './interfaces/error'
+const apiKey = 'd25d81b8-9986-4202-80da-b33a6c233580'
 
 // mongodb configuration
 const MongoClient = mongodb.MongoClient
@@ -19,19 +21,87 @@ client.connect(err => {
 })
 
 // expressjs configuration
-const port = 4220
+let port = 8080
 const host = '0.0.0.0'
 
+// take an argument to change the port for local development without docker
+const args = process.argv.slice(2)
+args.forEach((arg) => {
+  const split = arg.split('=')
+  const option = split[0]
+  const optionValue = parseInt(split[1])
+  // console.log('vars', option, optionValue, split)
+  if (option === 'port') {
+    port = optionValue
+  }
+})
+
+// configure the app
 const app = express()
+// set up cors for allowed origins
 app.use(cors({
   origin: [
+    // angular app port / docker angular port
     'http://localhost:4200',
+    // angular testing port
     'http://localhost:9876'
   ]
 }))
 
-// list the names in the db
+// names search function
+const searchNames = async function(searchTerm: string): Promise<any> {
+  const names = client.db('manage24').collection('names')
+  const cursor: mongodb.Cursor = names.find({ name: { $regex: searchTerm, $options: "$i" } })
+  try {
+    const result = await cursor.toArray()
+    console.log('result', result)
+    return Promise.resolve(result)
+  } catch (err) {
+    console.error(err)
+    return Promise.reject(err)
+  }
+}
+
+const checkKey = function(key: string): boolean {
+  return key === apiKey
+}
+
+// names search endpoint
 app.get('/names', (req: express.Request, res: express.Response) => {
+  const searchTerm = req.query.search
+  const key = req.query.apiKey
+
+  // reject if there is no sufficient api key
+  if (!key || !checkKey(key)) {
+    const error: Error = {
+      message: `You have supplied an invalid API key. Add your key to the 'key' paramater in the request URL, for example 'http://names-app.com/?key=<your-api-key>'`,
+      status: 403
+    }
+    res.status(403).send(error)
+  }
+
+  // reject if there is no searchterm
+  if (!searchTerm) {
+    const error: Error = {
+      message: `There must be a parameter of 'search' on the request URL, for example 'http://names-app.com?search=milly'`,
+      status: 500
+    }
+    res.status(500).send(error)
+  }
+
+  // finally, search the names
+  searchNames(searchTerm)
+    .then((data) => {
+      console.log('got data')
+      res.status(200).send(data)
+    }, (error: Error) => {
+      console.error('could not search the names', error.message)
+      res.status(500).send(error)
+    })
+})
+
+// list the names in the db - Old version created by mistake
+app.get('/all-names', (req: express.Request, res: express.Response) => {
   const names = client.db('manage24').collection('names')
   const cursor = names.find({})
   cursor.toArray()
@@ -73,54 +143,54 @@ app.listen(port, host)
 console.log('listening on ', port, host)
 
 const names = [
-  { name: "Paula Gowin" },
-  { name: "Lemuel Crown" },
-  { name: "Ellen Draughn" },
-  { name: "Alise Kaminsky" },
-  { name: "Sharleen Wesson" },
-  { name: "Janell Philpott" },
-  { name: "Verna Montanez" },
-  { name: "Rayna Branton" },
-  { name: "Paulita Perera" },
-  { name: "Priscila Pesce" },
-  { name: "Renae Pizano" },
-  { name: "Latarsha Mccallion" },
-  { name: "Myrna Millay" },
-  { name: "Marlen Gaynor" },
-  { name: "Mollie Ormand" },
-  { name: "Sunni Holtzman" },
-  { name: "Korey Vencill" },
-  { name: "Micheline Hopple" },
-  { name: "Regine Giron" },
-  { name: "Leonard Woodell" },
-  { name: "Bertie Guntrum" },
-  { name: "Frieda Weekly" },
-  { name: "Kendra Ragusa" },
-  { name: "Milly Shipman" },
-  { name: "Luciana Buttram" },
-  { name: "Maryjo Faulcon" },
-  { name: "Myriam Murrill" },
-  { name: "Nikia Carbonell" },
-  { name: "Estelle Anspach" },
-  { name: "Edie Delosreyes" },
-  { name: "Lorilee Knowles" },
-  { name: "Aracely Noakes" },
-  { name: "Alina Gould" },
-  { name: "Domitila Fralick" },
-  { name: "Amelia Wehner" },
-  { name: "Terrell Wilds" },
-  { name: "Cortney Vescio" },
-  { name: "Tommye Mcniel" },
-  { name: "Erich Hoy" },
-  { name: "Alissa Dunson" },
-  { name: "Dagmar Ayoub" },
-  { name: "Shane Bomba" },
-  { name: "Darcey Wulff" },
-  { name: "Robbie Tauber" },
-  { name: "Tawanna Cheek" },
-  { name: "Pamula Pettis" },
-  { name: "Paul Rucks" },
-  { name: "Lael Weber" },
-  { name: "Torrie Troupe" },
-  { name: "Etha Thornton" }
+  { name: 'Paula Gowin' },
+  { name: 'Lemuel Crown' },
+  { name: 'Ellen Draughn' },
+  { name: 'Alise Kaminsky' },
+  { name: 'Sharleen Wesson' },
+  { name: 'Janell Philpott' },
+  { name: 'Verna Montanez' },
+  { name: 'Rayna Branton' },
+  { name: 'Paulita Perera' },
+  { name: 'Priscila Pesce' },
+  { name: 'Renae Pizano' },
+  { name: 'Latarsha Mccallion' },
+  { name: 'Myrna Millay' },
+  { name: 'Marlen Gaynor' },
+  { name: 'Mollie Ormand' },
+  { name: 'Sunni Holtzman' },
+  { name: 'Korey Vencill' },
+  { name: 'Micheline Hopple' },
+  { name: 'Regine Giron' },
+  { name: 'Leonard Woodell' },
+  { name: 'Bertie Guntrum' },
+  { name: 'Frieda Weekly' },
+  { name: 'Kendra Ragusa' },
+  { name: 'Milly Shipman' },
+  { name: 'Luciana Buttram' },
+  { name: 'Maryjo Faulcon' },
+  { name: 'Myriam Murrill' },
+  { name: 'Nikia Carbonell' },
+  { name: 'Estelle Anspach' },
+  { name: 'Edie Delosreyes' },
+  { name: 'Lorilee Knowles' },
+  { name: 'Aracely Noakes' },
+  { name: 'Alina Gould' },
+  { name: 'Domitila Fralick' },
+  { name: 'Amelia Wehner' },
+  { name: 'Terrell Wilds' },
+  { name: 'Cortney Vescio' },
+  { name: 'Tommye Mcniel' },
+  { name: 'Erich Hoy' },
+  { name: 'Alissa Dunson' },
+  { name: 'Dagmar Ayoub' },
+  { name: 'Shane Bomba' },
+  { name: 'Darcey Wulff' },
+  { name: 'Robbie Tauber' },
+  { name: 'Tawanna Cheek' },
+  { name: 'Pamula Pettis' },
+  { name: 'Paul Rucks' },
+  { name: 'Lael Weber' },
+  { name: 'Torrie Troupe' },
+  { name: 'Etha Thornton' }
 ]
